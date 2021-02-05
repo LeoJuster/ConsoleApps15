939f6806bb33e6f4be24bcf888cbd3b22b63adc8
Leopold Juster <21900080@bucks.ac.uk>
2021-02-05 10:06:52 +0000

    Added Output heading

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -11,6 +11,7 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        public const int FEET_IN_MILES = 5280;
         private double miles;
         private double feet;
 
@@ -19,9 +20,18 @@ namespace ConsoleAppProject.App01
         /// </summary>
         public void Run()
         {
+            OutputHeading();
             InputMiles();
             CalculateFeet();
             OutputFeet();
+        }
+
+        private void OutputHeading()
+        {
+            Console.WriteLine("\n------------------------------");
+            Console.WriteLine("\n     Convert Miles to Feet    ");
+            Console.WriteLine("\n        By Leo Juster         ");
+            Console.WriteLine("\n------------------------------");
         }
 
         /// <summary>
