5ca568e81125e0278fa0a7617e58d74853a41d80
Leopold Juster <21900080@bucks.ac.uk>
2021-02-05 09:12:05 +0000

    Added code for InputMiles

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,4 +1,5 @@
-﻿namespace ConsoleAppProject.App01
+﻿using System;
+namespace ConsoleAppProject.App01
 {
     /// <summary>
     /// This class offers methods for converting a given
@@ -12,32 +13,40 @@
     {
         private double miles;
         private double feet;
+
         /// <summary>
-        /// 
+        /// The purpose of this method is to call the other methods
         /// </summary>
         public void Run()
         {
-
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
         }
+
         /// <summary>
         /// Prompts the user to enter the miles.
         /// Input miles as double number
         /// </summary>
+        /// 
         private void InputMiles()
         {
-
+            Console.Write("Please enter the number of miles >");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
         }
         /// <summary>
         /// 
         /// </summary>
+        /// 
         private void CalculateFeet()
         {
-
-
+            
         }
         /// <summary>
         /// 
         /// </summary>
+        /// 
         private void OutputFeet()
         {
 
